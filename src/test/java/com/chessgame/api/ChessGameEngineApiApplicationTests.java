aeb3bf961ca1d80a1ce7ff23abc4eb3424084ec8
Marlene Moraes <marlenevmoraes@outlook.com>
2026-03-13 22:40:51 -0300

    chore: initial project setup

---
+++ src/test/java/com/chessgame/api/ChessGameEngineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chessgame.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessGameEngineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
